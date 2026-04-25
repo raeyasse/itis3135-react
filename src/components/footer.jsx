794ac54f6308d908bace24245bdd4ca560b2a17d
raeyasse <raeytesfay@gmail.com>
2026-04-25 01:19:32 -0400

    corrected footer link to Vercel

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <Link to={"https://webpages.charlotte.edu/rassefa"}>CLT Web</Link> |{" "}
             <Link to={"https://raeyasse.github.io"}>GitHub.io</Link> |{" "}
             <Link to={"https://www.freecodecamp.org/rassefa"}>freeCodeCamp</Link> |{" "}
-            <Link to={"https://www.netflix.com"}>ITIS3135@Vercel</Link> |{" "}
+            <Link to={"https://itis3135-react-topaz.vercel.app/"}>ITIS3135@Vercel</Link> |{" "}
             <Link to={"https://www.linkedin.com/in/raey-asse/"}>LinkedIn</Link>
 
             <hr />
